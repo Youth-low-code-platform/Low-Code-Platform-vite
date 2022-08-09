3efdc85f54d6a82020a50378ffd6825d4c19a30b
D城兔Maziyo <247948681@qq.com>
2022-08-09 15:41:25 +0800

    chore: fix the btn name

--- src/utils/util.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentSchema } from '../types/lowCodeCompo.type'
-
-export function findIndex(component: ComponentSchema, components: ComponentSchema[]) {
-  return components.findIndex((item) => item === component)
-}
